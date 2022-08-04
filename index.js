6bb94c68f5e85bd3b3096d5c9807ee80351f0a74
Cici Marisa Hardianti <cicimarisahardianti@gmail.com>
2022-08-04 09:49:44 +0800

    Fix Review Jenny
    Fix Review Jenny

--- index.js
+++ index.js
@@ -5,8 +5,9 @@ const selectMode = document.getElementById("select-mode")
 
 function render() {
     let colorsHtml = colors.map((color) => {
+        let classBright = (!lightOrDark(color)) ? "light" : ""
         return `<div class="color-panel d-flex" style="background:${color}" aria-label="Hex Color display of ${color}">
-                    <p class="color-val">${color}</p>
+                    <p class="color-val ${classBright}">${color.substring(1)}</p>
                 </div>`
     }).join("")
 
@@ -16,17 +17,45 @@ function render() {
     const copiedHtml = document.getElementsByClassName("color-panel")
     const copiedHex = document.getElementsByClassName("color-val")
 
-    for (let i = 0; i < copiedHtml.length; i++){
+    for (let i = 0; i < copiedHtml.length; i++) {
         copiedHtml[i].addEventListener('click', function (event) {
             event.preventDefault()
-            navigator.clipboard.writeText(copiedHex[i].textContent).then(function() {
+            navigator.clipboard.writeText(copiedHex[i].textContent).then(function () {
                 alert(`${copiedHex[i].textContent} Copied`)
-              }, function() {
+            }, function () {
                 alert(`Unsuccessful`)
-              });
+            });
         });
     }
 }
+
+function lightOrDark(color) {
+    var r, g, b, hsp;
+
+    // If hex --> Convert it to RGB: http://gist.github.com/983661
+    color = +("0x" + color.slice(1).replace(
+        color.length < 5 && /./g, '$&$&'));
+
+    r = color >> 16;
+    g = color >> 8 & 255;
+    b = color & 255;
+
+    // HSP (Highly Sensitive Poo) equation from http://alienryderflex.com/hsp.html
+    hsp = Math.sqrt(
+        0.299 * (r * r) +
+        0.587 * (g * g) +
+        0.114 * (b * b)
+    );
+
+    // Using the HSP value, determine whether the color is light or dark
+    if (hsp > 127.5) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+
 
 function getColors(hexColor, colorMode) {
     fetch(`https://www.thecolorapi.com/scheme?hex=${hexColor}&mode=${colorMode}&count=5`)
